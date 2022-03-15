a783669b6bbd84ae8071b9f8c873eab34e3f02a5
Christian Sagel <christian.sagel@wooga.net>
2022-03-15 16:03:29 +0100

    Refactors the implementation of base properties in the default extension onto the base interface-now-trait (#39)

--- src/main/groovy/wooga/gradle/version/VersionPluginExtension.groovy
+++ src/main/groovy/wooga/gradle/version/VersionPluginExtension.groovy
@@ -18,6 +18,7 @@
 
 package wooga.gradle.version
 
+import com.wooga.gradle.BaseSpec
 import wooga.gradle.version.internal.release.base.ReleaseVersion
 import wooga.gradle.version.internal.release.base.TagStrategy
 import wooga.gradle.version.internal.release.semver.ChangeScope
@@ -25,49 +26,216 @@ import org.ajoberstar.grgit.Grgit
 import org.gradle.api.provider.Property
 import org.gradle.api.provider.Provider
 
-interface VersionPluginExtension {
+trait VersionPluginExtension implements BaseSpec {
 
-    Property<VersionScheme> getVersionScheme()
-    void versionScheme(VersionScheme value)
-    void versionScheme(Provider<VersionScheme> value)
-    void versionScheme(String value)
-    void setVersionScheme(VersionScheme value)
-    void setVersionScheme(Provider<VersionScheme> value)
-    void setVersionScheme(String value)
+    /**
+     * @return The version scheme being used by the plugin (such as SemVer2)
+     */
+    Property<VersionScheme> getVersionScheme() {
+        versionScheme
+    }
 
-    Property<VersionCodeScheme> getVersionCodeScheme()
-    void versionCodeScheme(VersionCodeScheme value)
-    void versionCodeScheme(Provider<VersionCodeScheme> value)
-    void versionCodeScheme(String value)
-    void setVersionCodeScheme(VersionCodeScheme value)
-    void setVersionCodeScheme(Provider<VersionCodeScheme> value)
-    void setVersionCodeScheme(String value)
+    private final Property<VersionScheme> versionScheme = objects.property(VersionScheme)
 
-    Property<Integer> getVersionCodeOffset()
-    void versionCodeOffset(Integer value)
-    void versionCodeOffset(Provider<Integer> value)
-    void setVersionCodeOffset(Integer value)
-    void setVersionCodeOffset(Provider<Integer> value)
+    void versionScheme(VersionScheme value) {
+        setVersionScheme(value)
+    }
 
-    Property<String> getReleaseBranchPattern()
-    void releaseBranchPattern(String value)
-    void releaseBranchPattern(Provider<String> value)
-    void setReleaseBranchPattern(String value)
-    void setReleaseBranchPattern(Provider<String> value)
+    void versionScheme(Provider<VersionScheme> value) {
+        setVersionScheme(value)
+    }
 
-    Property<String> getMainBranchPattern()
-    void mainBranchPattern(String value)
-    void mainBranchPattern(Provider<String> value)
-    void setMainBranchPattern(String value)
-    void setMainBranchPattern(Provider<String> value)
+    void setVersionScheme(VersionScheme value) {
+        versionScheme.set(value)
+    }
 
-    Property<Grgit> getGit()
+    void setVersionScheme(Provider<VersionScheme> value) {
+        versionScheme.set(value)
+    }
 
-    Provider<ReleaseVersion> getVersion()
-    Provider<Integer> getVersionCode()
+    void versionScheme(String value) {
+        setVersionScheme(value)
+    }
 
-    Provider<String> getStage()
-    Provider<ChangeScope> getScope()
+    void setVersionScheme(String value) {
+        versionScheme.set(VersionScheme.valueOf(value.trim()))
+    }
 
-    TagStrategy getTagStrategy()
+    /**
+     * @return The version code scheme being used by the plugin (such as SemVer2, releaseCount)
+     */
+    Property<VersionCodeScheme> getVersionCodeScheme() {
+        versionCodeScheme
+    }
+
+    private final Property<VersionCodeScheme> versionCodeScheme = objects.property(VersionCodeScheme)
+
+    void versionCodeScheme(VersionCodeScheme value) {
+        setVersionCodeScheme(value)
+    }
+
+    void versionCodeScheme(Provider<VersionCodeScheme> value) {
+        setVersionCodeScheme(value)
+    }
+
+    void setVersionCodeScheme(VersionCodeScheme value) {
+        versionCodeScheme.set(value)
+    }
+
+    void setVersionCodeScheme(Provider<VersionCodeScheme> value) {
+        versionCodeScheme.set(value)
+    }
+
+    void versionCodeScheme(String value) {
+        setVersionCodeScheme(value)
+    }
+
+    void setVersionCodeScheme(String value) {
+        versionCodeScheme.set(VersionCodeScheme.valueOf(value.trim()))
+    }
+
+    /**
+     * @return If set, used during the evaluation of {@code versionCode}
+     */
+    Property<Integer> getVersionCodeOffset(){
+        versionCodeOffset
+    }
+
+    private final Property<Integer> versionCodeOffset = objects.property(Integer)
+
+    void versionCodeOffset(Integer value) {
+        setVersionCodeOffset(value)
+    }
+
+    void versionCodeOffset(Provider<Integer> value) {
+        setVersionCodeOffset(value)
+    }
+
+    void setVersionCodeOffset(Integer value) {
+        versionCodeOffset.set(value)
+    }
+
+    void setVersionCodeOffset(Provider<Integer> value) {
+        versionCodeOffset.set(value)
+    }
+
+    /**
+     * @return If set, ....
+     */
+    Property<String> getReleaseBranchPattern() {
+        releaseBranchPattern
+    }
+
+    private final Property<String> releaseBranchPattern = objects.property(String)
+
+    void releaseBranchPattern(String value) {
+        setReleaseBranchPattern(value)
+    }
+
+    void releaseBranchPattern(Provider<String> value) {
+        setReleaseBranchPattern(value)
+    }
+
+    void setReleaseBranchPattern(String value) {
+        releaseBranchPattern.set(value)
+    }
+
+    void setReleaseBranchPattern(Provider<String> value) {
+        releaseBranchPattern.set(value)
+    }
+
+    /**
+     * @return If set, ...
+     */
+    Property<String> getMainBranchPattern() {
+        mainBranchPattern
+    }
+
+    private final Property<String> mainBranchPattern = objects.property(String)
+
+    void mainBranchPattern(String value) {
+        setMainBranchPattern(value)
+    }
+
+    void mainBranchPattern(Provider<String> value) {
+        setMainBranchPattern(value)
+    }
+
+    void setMainBranchPattern(String value) {
+        mainBranchPattern.set(value)
+    }
+
+    void setMainBranchPattern(Provider<String> value) {
+        mainBranchPattern.set(value)
+    }
+
+    /**
+     * @return The instance of the git client to be used
+     */
+    Property<Grgit> getGit() {
+        git
+    }
+
+    private final Property<Grgit> git = objects.property(Grgit)
+
+    /**
+     * @return The evaluated version of the project
+     */
+    Provider<ReleaseVersion> getVersion() {
+        version
+    }
+
+    private Provider<ReleaseVersion> version
+
+    void setVersion(Provider<ReleaseVersion> value){
+        version = value
+    }
+
+    /**
+     * @return The evaluated version code of the project
+     */
+    Provider<Integer> getVersionCode() {
+        versionCode
+    }
+
+    Provider<Integer> versionCode = objects.property(Integer)
+
+    void setVersionCode(Provider<Integer> value) {
+        versionCode = value
+    }
+
+    /**
+     * @return The stage to be used for evaluating the project version (such as rc, snapshot, final)
+     */
+    Provider<String> getStage() {
+        stage
+    }
+
+    Provider<String> stage = objects.property(String)
+
+    void setStage(Provider<String> value) {
+        stage = value
+    }
+
+    /**
+     * @return The scope to be used for evaluating the project version (such as major, minor, patch)
+     */
+    Provider<ChangeScope> getScope() {
+        scope
+    }
+
+    Provider<ChangeScope> scope = objects.property(ChangeScope)
+
+    void setScope(Provider<ChangeScope> value) {
+        scope = value
+    }
+
+    /**
+     * @return Used during nearest version inference
+     */
+    TagStrategy getTagStrategy() {
+        tagStrategy
+    }
+
+    TagStrategy tagStrategy = new TagStrategy()
 }
